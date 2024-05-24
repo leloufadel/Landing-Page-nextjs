b3f194b277bec3033b27534d41a80e5143add15d
leloufadel <leloufadel@gmail.com>
2024-05-24 14:03:04 +0000

    Create a toggle-menu

--- app/navbar/action-button.tsx
+++ app/navbar/action-button.tsx
@@ -1,7 +1,40 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Link  from "next/link";
+
+import { AlignJustify } from "lucide-react";
+
 const ActionButton = () => {
-    return (
-        <div className=""> Action Buttons</div>
-    );
-    }
-    
-    export default ActionButton;
+  return (
+    <>
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <AlignJustify />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetDescription>
+                <div className="flex flex-col space-y-4 items-start w-full mt-10 text-lg text-black">
+                  <Link href="/"> Sign In</Link>
+                  <Link href="/"> Get Started</Link>
+                  <Link href="/"> Features</Link>
+                  <Link href="/"> Pricing</Link>
+                  <Link href="/"> About</Link>
+                </div>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </>
+  );
+};
+
+export default ActionButton;
